789421bbca4b8c386b449eb929604fc710c98de4
junhyung001 <junhyung8325@gmail.com>
2022-12-29 03:26:25 +0900

    2022.12.29

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,49 +1,17 @@
-const messageList = document.querySelector("ul")
-const nickForm = document.querySelector("#nick")
-const messageForm = document.querySelector("#meassage")
-const socket = new WebSocket(`ws://${window.location.host}`)
+const socket = io();
 
-function makeMessage(type, payload){
-    const msg = {type, payload}
-    return JSON.stringify(msg);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg){
+    console.log(`The backend says: `, msg)
 }
 
-function handleOpen(){
-        console.log("Connected to Server ✅");
-}
-socket.addEventListener("open", handleOpen);
-
-socket.addEventListener("message", (message)=>{
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-})
-
-socket.addEventListener("close", ()=>{
-    console.log("Disconnected from Server X")
-});
-
-function handleSubmit(event){
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    const li = document.createElement("li");
-    li.innerText = `You: ${input.value}`;
-    messageList.append(li);
+    const input = form.querySelector("input")
+    socket.emit("enter_room", input.value, backendDone);
     input.value = ""
 }
 
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = ""
-}
-
-messageForm.addEventListener("submit", handleSubmit)
-nickForm.addEventListener("submit", handleNickSubmit)
-
-
-// setTimeout(() => {
-//     socket.send("Hello from the Browser")
-// }, 10000)
+form.addEventListener("submit", handleRoomSubmit)
--- src/server.js
+++ src/server.js
@@ -1,35 +1,35 @@
 import http from "http";
-import WebSocket from "ws";
+import {Server} from "socket.io"
 import express from "express";
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname+"/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.set("view engine", "pug");                              // 프론트앤드에서 pug를 활용해 프론트앤드 구현
+app.set("views", __dirname+"/views");                       // view를 구현할때 views폴더 안에 pug를 활용
+app.use("/public", express.static(__dirname + "/public"));  //plulic 폴더속에 있는 파일을 활용
+app.get("/", (req, res) => res.render("home"));             // req와 res 둘다 home으로 읽어옴
+app.get("/*", (req, res) => res.redirect("/"));             // res을 받아왔을때 only home으로 이동하는것을 작성
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);      //<-옆 방식은 express를 사용하는 http를 지원하는 것이고, ws를 사용하려면 다른 function을 사용해야 한다.
-const server = http.createServer(app);          //http서버 돌리기
-const wss = new WebSocket.Server({server});     //wss서버 돌리기
+const httpServer = http.createServer(app);          //http서버 돌리기
+const wsServer = new Server(httpServer);            //SoketIo의 내부함수를 활용해 무엇인가 만들고 outfut함
 
-// function handleConnection(socket){
-//     console.log(socket)
-// }
+wsServer.on("connection", socket =>{
+    socket.on("enter_room", (roomName, done)=> {
+        console.log(roomName);
+        setTimeout(() => {
+            done("hello from the backend");
+        }, 10000);
+    });
+});
 
-function onSocketClose(){
-    console.log("Disconnected from the Browser X");
-}
-
-const sockets = []
-
-wss.on("connection", (socket)=>{
+/*  const sockets = []
+    wss.on("connection", (socket)=>{
     sockets.push(socket)
     socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser X")      // server가 연결이 끊겼을때 consol.log를 찍을 수 있는 함수
+    });
     socket.on("message", (msg) =>{
         const message = JSON.parse(msg);
         switch(message.type){
@@ -39,6 +39,9 @@ wss.on("connection", (socket)=>{
                 socket["nickname"] = message.payload;
         }
     });
-});
+}); */
+//websocket을 활용한 함수 socket Io를 활용한 코드를 작성하기위한 주석
 
-server.listen(3000, handleListen)
+const handleListen = () => console.log(`Listening on http://localhost:3000`); // 함수가 호출되었을때 http://localhost:3000페이지를 생성함을 알려주는 log 
+
+httpServer.listen(3000, handleListen);
